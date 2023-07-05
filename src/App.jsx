ab3f0f9a3a82fcd8e71cd4da07c338f6d7a55c0d
Reinaldo Jose <reynaldojosedelacruzrojano@gmail.com>
2023-07-05 15:53:21 -0500

    Estado-activo

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,24 @@ function App() {
      
     }
     
-    
+
     navigator.geolocation.getCurrentPosition(success);
+    
   }, []);
 
 
   const [allData, setAllData] = useState();
+
   const [btn, setBtn] = useState(true);
+  const [backg,setBackf] = useState(true)
+
   const btnClick = () => {
     if (btn == true) {
       setBtn(false);
     } else {
       setBtn(true);
     }
+   // setBtn(!btn)
   };
 
 
@@ -37,24 +42,43 @@ function App() {
       .then((response) => {
         setAllData(response.data);
       })
-      .then((error) => console.log(error));
+      .catch((error) => console.log(error));
   }, [position]);
+
+  const degree = btn
+  ? `${(allData?.main.temp - 273.1).toFixed(0)} °C` 
+  :`${(((allData?.main.temp - 273.1) * 9) / 5 + 32).toFixed(0)} F`
+
+  const theme = backg
+  ? "background: radial-gradient(#D5F3FF 0%, #51B4E8 100%)"
+  : "background: radial-gradient(#53388f 0%, #2f2958 100%)"
   
+  const changeBacgr = () => { setBackf(!backg)}
   
-  
+
+document.body.style = theme 
   
   
   return (
     
-    <>
+    <main>
+     <div class = 'toggle-switch'>
+            <label>
+                <input type = 'checkbox'/>
+                <span onClick={changeBacgr} class = 'slider'></span>
+            </label>
+            <a href = 'https://dribbble.com/shots/14199649-Dark-Light-Mode-Toggle-Switch-Pattern-A11y'>
+                
+            </a>
+        </div>
    
     
     <div className="section_card">
       <h1>{allData?.name}</h1>
       <h2>{allData?.sys.country}</h2>
-      <img className="photo" src="https://openweathermap.org/img/wn/10d@2x.png" alt="" />
-      <h2>{(allData?.main.temp - 273.1).toFixed(0)} °C</h2>
-      <h2>{(((allData?.main.temp - 273.1) * 9) / 5 + 32).toFixed(0)} F</h2>
+      <img className="photo" src={`/${allData?.weather[0].icon.at(1)}.svg`} alt="" />
+      <h2>{degree}</h2>
+      
       
     </div>
       
@@ -62,7 +86,7 @@ function App() {
       <button className="Button" onClick={btnClick}>Grados {btn ? "C" : "F"}</button>
       </div>
    
-    </>
+    </main>
   );
 }
 
