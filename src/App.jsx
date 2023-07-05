556cf26608495be84786034a6d045d2ea4dd2adf
Reinaldo Jose <reynaldojosedelacruzrojano@gmail.com>
2023-07-04 19:33:59 -0500

    Estado-activo

--- src/App.jsx
+++ src/App.jsx
@@ -45,15 +45,25 @@ function App() {
   
   
   return (
+    
     <>
-      <h2>Wheather App</h2>
+   
+    
+    <div className="section_card">
       <h1>{allData?.name}</h1>
       <h2>{allData?.sys.country}</h2>
-      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="" />
-      <h2>{(allData?.main.temp - 273.1).toFixed(1)} c</h2>
-      <h2>{(((allData?.main.temp - 273.1) * 9) / 5 + 32).toFixed(1)} f</h2>
-      <button onClick={btnClick}>Grados {btn ? "C" : "F"}</button>
+      <img className="photo" src="https://openweathermap.org/img/wn/10d@2x.png" alt="" />
+      <h2>{(allData?.main.temp - 273.1).toFixed(0)} °C</h2>
+      <h2>{(((allData?.main.temp - 273.1) * 9) / 5 + 32).toFixed(0)} F</h2>
+      
+    </div>
+      
+      <div className="div">
+      <button className="Button" onClick={btnClick}>Grados {btn ? "C" : "F"}</button>
+      </div>
+   
     </>
   );
 }
+
 export default App;
